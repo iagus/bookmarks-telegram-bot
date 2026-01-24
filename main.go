b59b770753f886f3fe187525a11b7339dee1f17c
iagus <santiago.fgonz@gmail.com>
2026-01-24 16:36:11 +0800

    Builds basic JSON object with URL with the message string

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"fmt"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
 
 func main() {
-	fmt.Println("Hello world!");
+	bot, err := tgbotapi.NewBotAPI("<token>")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = false
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil && update.Message.From.UserName == "<user>" {
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			answer := fmt.Sprintf("{ \"url\": \"%s\" }", update.Message.Text)
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, answer)
+
+			bot.Send(msg)
+		}
+	}
 }
+
