5af0b57417eb3ab0492255362a78266ef392e63a
iagus <santiago.fgonz@gmail.com>
2026-02-14 11:12:39 +0800

    Add viewport meta tag

--- server/bookmarks.js
+++ server/bookmarks.js
@@ -76,6 +76,7 @@ const server = createServer(async (req, res) => {
         `
         <!DOCTYPE html>
           <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <link href="style.css" rel="stylesheet"></link>
           </head>
           <body>
