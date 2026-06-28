8eeb23b4b9e9575fb77c95c27d0cf05750dabe70
iagus <santiago.fgonz@gmail.com>
2026-06-28 15:58:26 +0800

    Modifies process func test after new implementation

---
+++ processor_test.go
@@ -0,0 +1,32 @@
+package main
+
+import(
+	"log"
+	"testing"
+	"regexp"
+)
+
+func TestProcess(t *testing.T) {
+	tags := []string{
+		"<meta property=\"og:title\" content=\"OG Title\"/>",
+		"<meta property=\"og:image\" content=\"OG Image\"/>",
+		"<meta property=\"og:description\" content=\"OG Description\"/>",
+	}
+
+	metadata, _ := process(tags)
+
+	wanted_title := regexp.MustCompile(`OG Title`)
+	wanted_img := regexp.MustCompile(`OG Image`)
+	wanted_desc := regexp.MustCompile(`OG Description`)
+
+	if !wanted_title.MatchString(metadata.Data.Title) {
+		t.Errorf("Title mismatch")
+	}
+	if !wanted_img.MatchString(metadata.Data.Image.URL) {
+		t.Errorf("Image URL mismatch")
+	}
+	if !wanted_desc.MatchString(metadata.Data.Description) {
+		t.Errorf("Description mismatch")
+	}
+}
+
